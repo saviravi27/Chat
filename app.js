dd6570424f0b6b7bb3fd643862894550b4f2032d
Savitha Kumari <savi.mdr@gmail.com>
2015-06-30 22:16:38 +1000

    Adding private messaging using /w

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 var morgan = require('morgan');
-var nicknames = [];
+var users = {};
 
 server.listen(3000);
 app.use(morgan('dev'));
@@ -18,32 +18,53 @@ io.sockets.on('connection',function(socket){
 	console.log('connection established for socket: ');
 	//console.log('connection established for socket: ', socket);
 	socket.on('new user',function(data,callback){
-		if(nicknames.indexOf(data) != -1){
+		if(data in users){
 			console.log(data + ' user already exist');
 			callback(false);
 		} else{
 			console.log(data + ' user added');
 			callback(true);
 			socket.nickname = data;
-			nicknames.push(socket.nickname);
+			users[socket.nickname] = socket;
 			updateUserList();
 		}
 	});
 
 	function updateUserList(){
-		io.sockets.emit('usernames',nicknames);
+		io.sockets.emit('usernames',Object.keys(users));
 	}
 
-	socket.on('chat msg',function(data){
-		io.sockets.emit('new msg', {nick : socket.nickname, msg : data});
+	socket.on('chat msg',function(data,callback){
+		var msg = data.trim();
+		if(msg.substr(0,3) ==='/w '){
+			msg = msg.substr(3);
+			var ind = msg.indexOf(' ');
+			if(ind != '-1'){
+				var name = msg.substring(0,ind);
+				var msg = msg.substring(ind+1);
+				if(name in users){
+					users[name].emit('Whisper', {nick : socket.nickname, msg : msg});
+					console.log('whisper');
+				}
+				else{
+					callback('Error : Enter valid username');
+				}	
+			}
+			else{
+				callback('Error : Please enter a message to whisper');
+			}
+		}
+		else{
+		io.sockets.emit('new msg', {nick : socket.nickname, msg : msg});
 		console.log('nick: '+socket.nickname+ '<br/>msg:'+data);
+	    }
 	});
 
 	socket.on('disconnect',function(data){
 		if(!socket.nickname){
 			return;
 		}
-		nicknames.splice(nicknames.indexOf(data),1);
+		delete users[socket.nickname];
 		updateUserList();
 		console.log(data + ' user disconnected');
 	});
